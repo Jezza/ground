304cb2810b0acf4be9499aee96f320e39edd994d
Jezza <jeremy.a.barrow@gmail.com>
2024-07-15 17:32:55 +0200

    Move cli (in commons) into ground-cli

--- crates/commons/src/lib.rs
+++ crates/commons/src/lib.rs
@@ -1,16 +1,2 @@
-#[cfg(all(
-    any(
-        feature = "cli-postgres",
-        feature = "cli-service",
-        feature = "cli-tracing",
-        feature = "cli-otel",
-    ),
-    any(
-        feature = "env",
-        feature = "clap",
-    )
-))]
-pub mod cli;
-
 #[cfg(feature = "tracing")]
 pub mod tracing;
